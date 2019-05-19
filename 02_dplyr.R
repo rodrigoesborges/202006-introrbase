f651624c3f9755f41482fd2ef52f19b2293ae3ec
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-05-19 03:23:34 -0300

    Inclui dicas na aula de dplyr e seus exercícios (cap 5 do r4ds)

---
+++ 02_dplyr.R
@@ -0,0 +1,118 @@
+# filter ----------------------------------------------------------------------
+
+# exercício 1
+# Quais filmes tiveram nota no imdb menor do que 5?
+
+
+# exercício 2
+# Há filmes com nota menor do que 5 e um orcamento maior que 1 milhão de dólares? 
+
+
+# exercício 3
+# E filmes com orçamento menor que 1 milhão de dólares e nota maior que 8?
+
+
+# exercício 4
+# Quais filmes usaram mais de um milhão de dólares para criar menos de 1h de telona?
+
+
+# exercício 5
+# Existiam filmes coloridos antes de 1940?
+
+# exercício 6
+# Quantos filmes de antes de 1940 eram em preto e branco?
+
+
+# exercício 7
+# Filtre apenas os filmes dirigidos por Walter Salles ou José Padilha
+
+
+# exercicio 8
+# Refaça o exercício 7 usando o operador '%in%'.
+
+
+# exercício 9
+# Quais filmes que não possuem informação tanto de receita quanto de orcamento.
+
+
+# exercício 10
+# Filtre apenas os filmes de Ação e Comédia com nota maior do que 8.
+
+
+# exercício 11
+# Pesquise sobre a função between. Quais filmes são da década de 1990?
+
+
+# arrange ---------------------------------------------------------------------
+
+# exercício 1
+# Ordene os filmes em ordem crescente de ano e decrescente de lucro
+
+
+# exercício 2 
+# Ordene por ordem decrescente do orçamento os filmes de um diretor a sua escolha
+
+
+# select ----------------------------------------------------------------------
+
+# exercício 1
+# Crie uma tabela com apenas as colunas titulo, diretor, e orcamento.
+
+
+# exercício 2
+# Remova as colunas ator_1, ator_2 e ator_3 de três formas diferentes
+
+
+# exercício 3
+# Crie uma tabela apenas com filmes do Spike Lee e as colunas titulo e ano
+# ordenada por ano.
+
+
+# mutate ----------------------------------------------------------------------
+
+# exercício 1
+# Crie uma variável chamada lucro. Salve em um objeto chamado imdb_lucro.
+
+
+# exercicio 2
+# Modifique a variável lucro para ficar na escala de milhões de dólares.
+
+
+# exercício 3
+# Filtre apenas os filmes com prejuízo maior do que 3 milhões de dólares. 
+# Os filmes com o maior prejuízo devem aparecer primeiro.
+
+
+# summarise -------------------------------------------------------------------
+
+# exercício 1
+# Use o `summarise` para calcular a proporção de filmes com diretoras.
+
+
+# exercício 2
+# Calcule a duração média e mediana dos filmes da base.
+
+
+# exercício 3
+# Calcule o lucro médio dos filmes com duracao < 60 minutos. E o lucro médio dos filmes com
+# mais de 2 horas.
+
+
+# group_by + summarise --------------------------------------------------------
+
+# exercício 1
+# Crie uma tabela com apenas o nome dos diretores com mais de 10 filmes.
+
+
+# exercício 2
+# Crie uma tabela com a receita média e mediana dos filmes por ano.
+
+
+# exercício 3
+# Crie uma tabela com a maior e a menor nota dos filmes por tipo de classificação.
+
+
+# uniao -----------------------------------------------------------------------
+
+# exercicio 1
+
