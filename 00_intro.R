238642465bd99ad331598206c2b5de07d7279d2b
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-05-20 01:30:31 -0300

    exercicios e versao usada da aula de intro

--- 00_intro.R
+++ 00_intro.R
@@ -6,30 +6,70 @@
 # 3. Qual a classe dos objetos criados?
 
 # Vetores ---------------------------------------------------------------------
-# 1. Crie um objeto que tenha o seu nome e o nome de um colega
+# 1. Crie um objeto (nomes) que tenha o seu nome e o nome de um colega
 
-# 2. Crie um objeto que tenha a sua altura e a altura de um colega
+# 2. Crie um objeto (alturas) que tenha a sua altura e a altura de um colega
 
 # 3. Selecione apenas a maior altura. Faça o mesmo com a menor.
 
-# 4. Qual a altura média das alturas?
+# 4. Qual a altura média das alturas? use "+" para soma e "/" para divisão
 
-# 5. Selecione apenas as  alturas maiores do que 1.7 metros
+# 5. Selecione o seu nome no vetor "nomes" de duas formas diferentes
 
 
 # Comparações lógicas e numéricas ---------------------------------------------
-# 1. 
+# Para os exercicios desta parte, use o vetor "valores"
+set.seed(123)
+valores <- rnorm(10, 1.75, 0.08)
 
-# 2. 
+# 1. Todos os valores são maiores do que 1.60?
 
-# 3.  
+
+# 2. Há algum valor maior do que 1.90?
+
+
+# 3. Selecione apenas os valores maiores do que 1.7 metros.
+
+
+# 4. Selecione apenas os valores maiores que 1.70 e menores do que 1.80  
+
+
+# 5. Selecione apenas os valores menores que 1.70 ou maiores do que 1.80   
+
+# Use o novo vetor valores para o próximo exercício
+valores[c(3, 6, 8, 9)] <- NA
+valores
+
+# 6. Selecione apenas os valores que não são NA em "valores".
+# salve o resultado em valores2
+
+
+
+# Operações vetoriais ---------------------------------------------------------
+# 1. Descubra a raiz de cada uma das alturas salvas em valores2. Veja ?sqrt
+
+
+# 2. Dê mais 5 centímetros de altura a todos
+
+
+# 3. Qual a proporção de casos de valores2 são maiores do que 1.75?
+# Resolva usando sum() e mean(). Por que isso acontece?
+
+
 
 # Funções ---------------------------------------------------------------------
-# 1. 
+# O IMC é um indicador de massa corporal, cuja fórmula 
+# é "massa / (altura em metros ao quadrado)"
+# Ex: 80 quilos e 2 metros de altura: 80 / 4 = 20
+# 1. Crie uma função que, dado peso e altura, calcula o IMC. 
+
+
 
 # Controle de fluxo -----------------------------------------------------------
-# 1. 
+# 1. Para cada valor em "valores2", imprima "alto" ou "baixo" caso o valor
+# esteja abaixo ou acima de um valor a sua escolha. Use o loop.
 
-# 2. 
 
-# 3. 
+# 2. Tente resolver o problema acima de forma vetorizada.
+
+
