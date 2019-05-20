97dff0f063af35045da4519b1c180a8396f4f98b
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-05-19 21:10:01 -0300

    exericios aula de intro

---
+++ 00_intro.R
@@ -0,0 +1,35 @@
+# Objetos ---------------------------------------------------------------------
+# 1. Salve a informação do seu nome em um objeto chamado nome
+
+# 2. Defina um objeto altura com sua altura em metros
+
+# 3. Qual a classe dos objetos criados?
+
+# Vetores ---------------------------------------------------------------------
+# 1. Crie um objeto que tenha o seu nome e o nome de um colega
+
+# 2. Crie um objeto que tenha a sua altura e a altura de um colega
+
+# 3. Selecione apenas a maior altura. Faça o mesmo com a menor.
+
+# 4. Qual a altura média das alturas?
+
+# 5. Selecione apenas as  alturas maiores do que 1.7 metros
+
+
+# Comparações lógicas e numéricas ---------------------------------------------
+# 1. 
+
+# 2. 
+
+# 3.  
+
+# Funções ---------------------------------------------------------------------
+# 1. 
+
+# Controle de fluxo -----------------------------------------------------------
+# 1. 
+
+# 2. 
+
+# 3. 
